e176dab6eb60b2915be3622f8034f34f7ea52289
LoLo <codgodfox3@gmail.com>
2024-12-05 19:00:08 +0300

    bobrinok

--- src/linear.clj
+++ src/linear.clj
@@ -1,2 +1,50 @@
 (ns linear)
 
+(defn operation [f & vector]
+  (apply mapv f vector))
+
+(defn v+ [& vectors]
+  (apply operation + vectors))
+
+(defn v- [& vectors]
+  (apply operation - vectors))
+
+(defn v* [& vectors]
+  (apply operation * vectors))
+
+(defn vd [& vectors]
+  (apply operation / vectors))
+
+(defn dot [& vectors]
+  (if (empty? vectors) 0
+  (reduce + (apply v* vectors))))
+
+(defn v*s [vector & scalars]
+  (let [prod (apply * scalars)]
+    (operation #(* % prod) vector)))
+
+(defn m+ [& matrices]
+  (apply operation v+ matrices))
+
+(defn m- [& matrices]
+  (apply operation v- matrices))
+
+(defn m* [& matrices]
+  (apply operation v* matrices))
+
+(defn md [& matrices]
+  (apply operation vd matrices))
+
+(defn m*s [matrices & scalars]
+  (let [prod (apply * scalars)]
+    (operation #(operation (fn [param1] (* param1 prod)) %) matrices)))
+
+(defn transpose [matrices]
+  (apply operation vector matrices))
+
+
+(defn m*v [matrices vector]
+  (operation #(dot vector %) matrices))
+
+(defn m*m [& matrices]
+  (reduce  (fn [A, B] (operation #(m*v (transpose B) %) A)) matrices))
